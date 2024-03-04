f044e866e95bd268f40966e06559540dc8e9d5d0
Gabriel Mendes Tury Guimarães <80724636+GabrielTury@users.noreply.github.com>
2024-03-04 10:38:22 -0300

    Initial Commit

---
+++ Assets/InputController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int i = 0;
+        while(i < Input.touchCount)
+        {
+            Touch touch = Input.GetTouch(i);
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider != null)
+                {
+                    Debug.Log("Finger"+ i + " "+hit.collider.name);
+                    hit.collider.SendMessage("Touched", SendMessageOptions.DontRequireReceiver); //Executa a função que ta em string
+                }
+            }
+        }
+    }
+
+   /* public void OnGUI()
+    {
+        int i = 0;
+
+        while(i < Input.touchCount)
+        {
+            GUI.Label(new Rect(0,i *40,200,40), "Touch index" + i + ":" + Input.touchCount);
+            i++;
+        }
+    }*/
+}
