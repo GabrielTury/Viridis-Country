aa15f93594df7b8e638057c4d4cc74e74f79aa78
Castanha <69070402+Castanha@users.noreply.github.com>
2024-04-21 23:08:58 -0300

    Sound Effects Organizados

--- Assets/Viridis Country/Scripts/Other/GameEvents.cs
+++ Assets/Viridis Country/Scripts/Other/GameEvents.cs
@@ -16,6 +16,29 @@ namespace GameEventSystem
         public static event UnityAction <AudioManager.ConstructionAudioTypes> Construction_Removed;
         public static void OnConstructionRemoved(AudioManager.ConstructionAudioTypes cType) => Construction_Removed?.Invoke(cType);
 
+
+
+
+        public static event UnityAction<AudioManager.SoundEffects> Zoom;
+        public static void OnZoom(AudioManager.SoundEffects cType) => Zoom?.Invoke(cType);
+
+        public static event UnityAction<AudioManager.SoundEffects> Click;
+        public static void OnClick(AudioManager.SoundEffects cType) => Click?.Invoke(cType);
+
+        public static event UnityAction<AudioManager.SoundEffects> Select;
+        public static void OnSelect(AudioManager.SoundEffects cType) => Select?.Invoke(cType);
+
+        public static event UnityAction<AudioManager.SoundEffects> OneStar;
+        public static void OnOneStar(AudioManager.SoundEffects cType) => OneStar?.Invoke(cType);
+
+        public static event UnityAction<AudioManager.SoundEffects> TwoStar;
+        public static void OnTwoStar(AudioManager.SoundEffects cType) => TwoStar?.Invoke(cType);
+
+        public static event UnityAction<AudioManager.SoundEffects> ThreeStar;
+        public static void OnThreeStar(AudioManager.SoundEffects cType) => ThreeStar?.Invoke(cType);
+
+
+
         public static event UnityAction Level_Start;
         public static void OnLevelStart() => Level_Start?.Invoke();
 
